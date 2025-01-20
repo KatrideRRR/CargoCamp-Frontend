3d1d9db7ba2b70a92d8365805791d3fe334c6175
KatrideRRR <akimadzhiametov@gmail.com>
2025-01-20 14:25:51 +0300

    Updated .css files of Navbar.js and BottomMenu.js

--- frontend/src/components/BottomMenu.js
+++ frontend/src/components/BottomMenu.js
@@ -3,33 +3,27 @@ import { Link } from 'react-router-dom';
 import '../styles/BottomMenu.css';
 
 const BottomMenu = () => {
-  return (
-    <div className="bottom-menu">
-      <div className="menu-left">
-        <Link to="/orders" className="menu-item">
-          Все заказы
-        </Link>
-        <Link to="/special-orders" className="menu-item">
-          Спецзаказы
-        </Link>
-      </div>
+    return (
+        <div className="bottom-menu">
+            <div className="menu-left">
+                <Link to="/orders" className="menu-item">
+                    Все заказы
+                </Link>
+            </div>
 
-      <div className="menu-center">
-        <Link to="/create-order" className="menu-plus">
-          +
-        </Link>
-      </div>
+            <div className="menu-center">
+                <Link to="/create-order" className="menu-plus">
+                    +
+                </Link>
+            </div>
 
-      <div className="menu-right">
-        <Link to="/massage" className="menu-item">
-          Сообщения
-        </Link>
-        <Link to="/active-orders" className="menu-item">
-          Активные заказы
-        </Link>
-      </div>
-    </div>
-  );
+            <div className="menu-right">
+                <Link to="/active-orders" className="menu-item">
+                    Активные заказы
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default BottomMenu;
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,49 +1,25 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import  '../styles/Navbar.css';
 
 
 const Navbar = () => {
-
     const navigate = useNavigate();
 
     const handleProfileClick = () => {
-
-            navigate('/profile'); // Если пользователь авторизован, переходим на страницу профиля
-
+        navigate('/profile');
     };
 
     return (
-        <nav
-            style={{
-                position: 'fixed',
-                top: 0,
-                width: '100%',
-                backgroundColor: '#f8f8f8',
-                padding: '10px',
-                zIndex: 1000,
-                display: 'flex',
-                justifyContent: 'space-between',
-            }}
-        >
-            <div>
-                <Link to="/" style={{ marginRight: '15px' }}>Home</Link>
+        <nav className="navbar">
+            <div className="navbar-left">
+                <Link to="/" className="navbar-item">
+                    Home
+                </Link>
             </div>
-            <div>
-                <button
-                    onClick={handleProfileClick}
-                    style={{
-                        textDecoration: 'none',
-                        marginRight: 30,
-                        color: 'blue',
-                        background: 'none',
-                        border: 'none',
-                        cursor: 'pointer',
-                        fontSize: '16px',
-                    }}
-                >
-                    Profile
-                </button>
-            </div>
+            <button className="navbar-button" onClick={handleProfileClick}>
+                Profile
+            </button>
         </nav>
     );
 };
