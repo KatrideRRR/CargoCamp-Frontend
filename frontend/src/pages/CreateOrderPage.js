e20d7d88ffd8ce7a504af227458ea943cb607b3f
KatrideRRR <akimadzhiametov@gmail.com>
2025-02-08 18:57:46 +0300

    fixed styles of create orders and profile

--- frontend/src/pages/CreateOrderPage.js
+++ frontend/src/pages/CreateOrderPage.js
@@ -156,154 +156,156 @@ function CreateOrderPage({ currentUserId }) {
     };
 
     return (
-        <YMaps query={{ apikey: "bf97867b-5ffb-4fc4-9fd5-8997874b300e" }}>
-            <div className="container">
-                <div className="form-container">
-                    {error && <p className="error-text">{error}</p>}
-                    <form onSubmit={handleSubmit} className="form">
-                        <div className="input-group">
-                            <label className="label">Тип заказа</label>
-                            <input
-                                className="input"
-                                type="text"
-                                value={formData.type}
-                                onChange={(e) => handleTypeInputChange(e.target.value)}
-                                placeholder="Введите тип заказа..."
-                                required
-                            />
-                            {suggestions.length > 0 && (
-                                <ul className="suggestions">
-                                    {suggestions.map((suggestion, index) => (
-                                        <li
-                                            key={index}
-                                            className="suggestion-item"
-                                            onClick={() => handleSuggestionClick(suggestion)}
-                                        >
-                                            {suggestion}
-                                        </li>
-                                    ))}
-                                </ul>
-                            )}
-                        </div>
-
-                        <div className="input-group">
-                            <label className="label">Описание работы</label>
-                            <textarea
-                                className="textarea"
-                                placeholder="Введите описание работы"
-                                value={formData.description}
-                                onChange={handleDescriptionChange}
-                                rows="3" // Начальная высота
-                            />
-                        </div>
-
-                        <div className="input-group">
-                            <label className="label">Адрес</label>
-                            <input
-                                className="input"
-                                type="text"
-                                placeholder="Введите адрес"
-                                value={formData.address}
-                                onChange={handleAddressChange}
-                                required
-                            />
-                        </div>
-
-                        <div className="map-container">
-                            <Map
-                                defaultState={{center: [44.9572, 34.1108], zoom: 10}}
-                                style={{width: "100%", height: "300px"}}
-                            >
-                                {markerPosition && <Placemark geometry={markerPosition}/>}
-                            </Map>
-                        </div>
-
-                        <div className="input-row">
-                            <div className="input-group date-picker">
-                                <label className="label">Дата и время</label>
-                                <DatePicker
-                                    selected={formData.workTime}
-                                    onChange={(date) => setFormData({...formData, workTime: date})}
-                                    showTimeSelect
-                                    timeFormat="HH:mm"
-                                    timeIntervals={15}
-                                    dateFormat="Pp"
-                                    placeholderText="Выберите дату и время"
-                                    minDate={new Date()}
-                                    minTime={getMinTime(formData.workTime)}
-                                    maxTime={new Date(0, 0, 0, 23, 59, 59)}
-                                    className="input"
-                                    portalId="date-picker-portal"
-                                    popperProps={{
-                                        modifiers: [
-                                            {
-                                                name: "preventOverflow",
-                                                options: {
-                                                    boundary: "viewport",
-                                                },
-                                            },
-                                            {
-                                                name: "offset",
-                                                options: {
-                                                    offset: [0, 8],
-                                                },
-                                            },
-                                        ],
-                                    }}
-                                />
-
-
-                            </div>
+        <YMaps query={{apikey: "bf97867b-5ffb-4fc4-9fd5-8997874b300e"}}>
+            <div className="create-order-page">
+                <div className="container">
+                    <div className="form-container">
+                        {error && <p className="error-text">{error}</p>}
+                        <form onSubmit={handleSubmit} className="form">
                             <div className="input-group">
-                                <label className="label">Предложенная сумма</label>
+                                <label className="label">Тип заказа</label>
                                 <input
                                     className="input"
-                                    type="number"
-                                    placeholder="Введите сумму"
-                                    value={formData.proposedSum}
-                                    onChange={(e) =>
-                                        setFormData({...formData, proposedSum: e.target.value})
-                                    }
+                                    type="text"
+                                    value={formData.type}
+                                    onChange={(e) => handleTypeInputChange(e.target.value)}
+                                    placeholder="Введите тип заказа..."
+                                    required
+                                />
+                                {suggestions.length > 0 && (
+                                    <ul className="suggestions">
+                                        {suggestions.map((suggestion, index) => (
+                                            <li
+                                                key={index}
+                                                className="suggestion-item"
+                                                onClick={() => handleSuggestionClick(suggestion)}
+                                            >
+                                                {suggestion}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                )}
+                            </div>
+
+                            <div className="input-group">
+                                <label className="label">Описание работы</label>
+                                <textarea
+                                    className="textarea"
+                                    placeholder="Введите описание работы"
+                                    value={formData.description}
+                                    onChange={handleDescriptionChange}
+                                    rows="3" // Начальная высота
+                                />
+                            </div>
+
+                            <div className="input-group">
+                                <label className="label">Адрес</label>
+                                <input
+                                    className="input"
+                                    type="text"
+                                    placeholder="Введите адрес"
+                                    value={formData.address}
+                                    onChange={handleAddressChange}
                                     required
                                 />
                             </div>
-                        </div>
-                        {/* Блок загрузки изображений */}
-                        <div className="file-upload-container">
-                            <label htmlFor="file-input" className="file-input-label">
-                                Загрузить изображения
-                            </label>
-                            <input
-                                id="file-input"
-                                type="file"
-                                className="file-input"
-                                multiple
-                                accept="image/*"
-                                onChange={handleImageChange}
-                            />
-                        </div>
 
-                        {/* Область предпросмотра загруженных изображений */}
-                        <div className="image-preview">
-                            {images.length > 0 ? (
-                                images.map((image, index) => (
-                                    <img key={index} src={URL.createObjectURL(image)} alt={`Preview ${index + 1}`}
-                                         className="image-preview-item"/>
-                                ))
-                            ) : (
-                                <p className="no-image-text">Изображения не выбраны</p>
-                            )}
-                        </div>
+                            <div className="map-container">
+                                <Map
+                                    defaultState={{center: [44.9572, 34.1108], zoom: 10}}
+                                    style={{width: "100%", height: "300px"}}
+                                >
+                                    {markerPosition && <Placemark geometry={markerPosition}/>}
+                                </Map>
+                            </div>
 
-                <button type="submit" className="submit-button">
-                    Создать заказ
-                </button>
-            </form>
-        </div>
-</div>
-</YMaps>
+                            <div className="input-row">
+                                <div className="input-group date-picker">
+                                    <label className="label">Дата и время</label>
+                                    <DatePicker
+                                        selected={formData.workTime}
+                                        onChange={(date) => setFormData({...formData, workTime: date})}
+                                        showTimeSelect
+                                        timeFormat="HH:mm"
+                                        timeIntervals={15}
+                                        dateFormat="Pp"
+                                        placeholderText="Выберите дату и время"
+                                        minDate={new Date()}
+                                        minTime={getMinTime(formData.workTime)}
+                                        maxTime={new Date(0, 0, 0, 23, 59, 59)}
+                                        className="input"
+                                        portalId="date-picker-portal"
+                                        popperProps={{
+                                            modifiers: [
+                                                {
+                                                    name: "preventOverflow",
+                                                    options: {
+                                                        boundary: "viewport",
+                                                    },
+                                                },
+                                                {
+                                                    name: "offset",
+                                                    options: {
+                                                        offset: [0, 8],
+                                                    },
+                                                },
+                                            ],
+                                        }}
+                                    />
+
+
+                                </div>
+                                <div className="input-group">
+                                    <label className="label">Предложенная сумма</label>
+                                    <input
+                                        className="input"
+                                        type="number"
+                                        placeholder="Введите сумму"
+                                        value={formData.proposedSum}
+                                        onChange={(e) =>
+                                            setFormData({...formData, proposedSum: e.target.value})
+                                        }
+                                        required
+                                    />
+                                </div>
+                            </div>
+                            {/* Блок загрузки изображений */}
+                            <div className="file-upload-container">
+                                <label htmlFor="file-input" className="file-input-label">
+                                    Загрузить изображения
+                                </label>
+                                <input
+                                    id="file-input"
+                                    type="file"
+                                    className="file-input"
+                                    multiple
+                                    accept="image/*"
+                                    onChange={handleImageChange}
+                                />
+                            </div>
+
+                            {/* Область предпросмотра загруженных изображений */}
+                            <div className="image-preview">
+                                {images.length > 0 ? (
+                                    images.map((image, index) => (
+                                        <img key={index} src={URL.createObjectURL(image)} alt={`Preview ${index + 1}`}
+                                             className="image-preview-item"/>
+                                    ))
+                                ) : (
+                                    <p className="no-image-text">Изображения не выбраны</p>
+                                )}
+                            </div>
+
+                            <button type="submit" className="submit-button">
+                                Создать заказ
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </YMaps>
 )
-    ;
+;
 }
 
 export default CreateOrderPage;
